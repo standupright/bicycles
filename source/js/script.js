11af14f7d420f46f6aa854668cf6319f4b4dc537
standupright <55686549+standupright@users.noreply.github.com>
2021-08-24 13:23:37 +0500

    finish validation form

--- source/js/script.js
+++ source/js/script.js
@@ -4,6 +4,10 @@ const nav = document.querySelector('.nav');
 const navToggle = document.querySelector('.nav__toggle');
 const body = document.querySelector('#body');
 
+const form = document.querySelector('.form-field');
+const nameForm = document.querySelector('#name');
+const telForm = document.querySelector('#tel');
+
 // nav open and closed
 nav.classList.remove('nav--nojs');
 nav.classList.add('nav--closed');
@@ -40,3 +44,52 @@ for (let smoothLink of smoothLinks) {
         }
     });
 };
+
+// Валидация формы
+nameForm.addEventListener('invalid', () => {
+  if (nameForm.validity.valueMissing) {
+    nameForm.setCustomValidity('Обязательное поле');
+  } else {
+    nameForm.setCustomValidity('');
+  }
+});
+
+telForm.addEventListener('invalid', () => {
+  if (telForm.validity.valueMissing || telForm.validity.patternMismatch) {
+    telForm.setCustomValidity('Неверный номер телефона');
+  } else {
+    telForm.setCustomValidity('');
+  }
+});
+
+let isStorageSupport = true;
+let storageName = "";
+let storageTel = "";
+
+try {
+  storageName = localStorage.getItem("name");
+  storageTel = localStorage.getItem("tel");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+if (storageName) {
+  nameForm.value = storageName;
+}
+
+if (storageTel) {
+  telForm.value = storageTel;
+}
+
+// Событие отправки формы
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+
+  if (isStorageSupport) {
+    localStorage.setItem("name", nameForm.value);
+    localStorage.setItem("tel", telForm.value);
+  }
+
+  nameForm.value = '';
+  telForm.value = '';
+});
