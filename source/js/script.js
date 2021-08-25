e6199f5582d0349b5075fe5cbcc4f158f0d6a36f
standupright <55686549+standupright@users.noreply.github.com>
2021-08-25 14:26:12 +0500

    fix main errors

--- source/js/script.js
+++ source/js/script.js
@@ -1,64 +1,77 @@
+const NAV_CLOSED = 'nav--closed';
+const NAV_NOJS = 'nav--nojs';
+const NAV_OPENED = 'nav--opened';
+const OVERFLOW = 'overflow-hidden';
 const nav = document.querySelector('.nav');
 const navToggle = document.querySelector('.nav__toggle');
 const body = document.querySelector('#body');
-
 const form = document.querySelector('.form-field');
 const nameForm = document.querySelector('#name');
 const telForm = document.querySelector('#tel');
 
 // nav open and closed
-nav.classList.remove('nav--nojs');
-nav.classList.add('nav--closed');
+if (nav) {
+  nav.classList.remove(NAV_NOJS);
+  nav.classList.add(NAV_CLOSED);
+}
 
-navToggle.addEventListener('click', () => {
-  if (nav.classList.contains('nav--closed')) {
-    nav.classList.remove('nav--closed');
-    nav.classList.add('nav--opened');
-    body.classList.add('overflow-hidden');
-  } else {
-    nav.classList.add('nav--closed');
-    nav.classList.remove('nav--opened');
-    body.classList.remove('overflow-hidden');
-  }
-});
-
-// Плавный скролл
-const smoothLinks = document.querySelectorAll('a[href^="#"]');
-for (const smoothLink of smoothLinks) {
-  smoothLink.addEventListener('click', (evt) => {
-    evt.preventDefault();
-    const id = smoothLink.getAttribute('href');
-
-    const placeScroll = document.querySelector(id);
-
-    if (placeScroll) {
-      placeScroll.scrollIntoView({
-        behavior: 'smooth',
-        block: 'start',
-      });
-      nav.classList.remove('nav--opened');
-      nav.classList.add('nav--closed');
-      body.classList.remove('overflow-hidden');
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    if (nav.classList.contains(NAV_CLOSED)) {
+      nav.classList.remove(NAV_CLOSED);
+      nav.classList.add(NAV_OPENED);
+      body.classList.add(OVERFLOW);
+    } else {
+      nav.classList.add(NAV_CLOSED);
+      nav.classList.remove(NAV_OPENED);
+      body.classList.remove(OVERFLOW);
     }
   });
 }
 
-// Валидация формы
-nameForm.addEventListener('invalid', () => {
-  if (nameForm.validity.valueMissing) {
-    nameForm.setCustomValidity('Обязательное поле');
-  } else {
-    nameForm.setCustomValidity('');
-  }
-});
+// Плавный скролл
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (const smoothLink of smoothLinks) {
+  if (smoothLink) {
+    smoothLink.addEventListener('click', (evt) => {
+      evt.preventDefault();
+      const id = smoothLink.getAttribute('href');
 
-telForm.addEventListener('invalid', () => {
-  if (telForm.validity.valueMissing || telForm.validity.patternMismatch) {
-    telForm.setCustomValidity('Неверный номер телефона');
-  } else {
-    telForm.setCustomValidity('');
+      const placeScroll = document.querySelector(id);
+
+      if (placeScroll) {
+        placeScroll.scrollIntoView({
+          behavior: 'smooth',
+          block: 'start',
+        });
+        nav.classList.remove(NAV_OPENED);
+        nav.classList.add(NAV_CLOSED);
+        body.classList.remove(OVERFLOW);
+      }
+    });
   }
-});
+}
+
+// Валидация формы
+if (nameForm) {
+  nameForm.addEventListener('invalid', () => {
+    if (nameForm.validity.valueMissing) {
+      nameForm.setCustomValidity('Обязательное поле');
+    } else {
+      nameForm.setCustomValidity('');
+    }
+  });
+}
+
+if (telForm) {
+  telForm.addEventListener('invalid', () => {
+    if (telForm.validity.valueMissing || telForm.validity.patternMismatch) {
+      telForm.setCustomValidity('Неверный номер телефона');
+    } else {
+      telForm.setCustomValidity('');
+    }
+  });
+}
 
 let isStorageSupport = true;
 let storageName = '';
@@ -71,23 +84,25 @@ try {
   isStorageSupport = false;
 }
 
-if (storageName) {
+if (storageName && nameForm) {
   nameForm.value = storageName;
 }
 
-if (storageTel) {
+if (storageTel && telForm) {
   telForm.value = storageTel;
 }
 
 // Событие отправки формы
-form.addEventListener('submit', (evt) => {
-  evt.preventDefault();
+if (form) {
+  form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
 
-  if (isStorageSupport) {
-    localStorage.setItem('name', nameForm.value);
-    localStorage.setItem('tel', telForm.value);
-  }
+    if (isStorageSupport) {
+      localStorage.setItem('name', nameForm.value);
+      localStorage.setItem('tel', telForm.value);
+    }
 
-  nameForm.value = '';
-  telForm.value = '';
-});
+    nameForm.value = '';
+    telForm.value = '';
+  });
+}
